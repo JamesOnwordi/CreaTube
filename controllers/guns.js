0c8abd776057970bde4ec27b45f43b0df707cf83
JamesOnwordi <Jamesonwordi50@gmail.com>
2022-09-14 18:17:14 -0500

    Users model added

--- controllers/guns.js
+++ controllers/guns.js
@@ -1,15 +1,37 @@
 const express = require("express")
 const router = express.Router()
 const db = require("../models")
+router.use(express.urlencoded({extended:false}))
 
 
-// new model
 router.get("/",(req,res)=>{
-    res.render("new")
+    res.render("projects/home")
 })
 
-router.post("/",(req,res)=>{
+// new model
+router.get("/new",(req,res)=>{
+    res.render("projects/new")
+    
+})
+
+router.post("/new",(req,res)=>{
     // added
+    console.log(req.body)
+    // add new model to db
+    res.redirect("/guns")
+})
+
+
+router.get("/update",(req,res)=>{
+    res.render("projects/update")
+    
+})
+
+router.post("/update",(req,res)=>{
+    // added
+    console.log(req.body)
+    // add new model to db
+    res.redirect("/updates")
 })
 
 module.exports = router
--- index.js
+++ index.js
@@ -1,11 +1,36 @@
+require('dotenv').config()
 const express = require("express")
 const axios = require("axios")
 const expressEjsLayouts = require("express-ejs-layouts")
+const cookieParser = require('cookie-parser')
 const db = require("./models")
+const crypto = require('crypto-js')
 const app = express()
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 app.set("view engine","ejs")
 app.use(expressEjsLayouts)
+app.use(express.urlencoded({extended:false}))
+app.use(cookieParser())
+
+// our custom auth middleware
+app.use(async (req, res, next) => {
+    // console.log('hello from a middleware 👋')
+    // if there is a cookie on the incoming request
+    if (req.cookies.userId) {
+        // decrypt the user id before we look up the user in the db
+        const decryptedId = crypto.AES.decrypt(req.cookies.userId.toString(), process.env.ENC_SECRET)
+        const decryptedIdString = decryptedId.toString(crypto.enc.Utf8)
+        // look up the user in the db
+        const user = await db.user.findByPk(decryptedIdString)
+        // mount the user on the res.locals
+        res.locals.user = user
+    // if there is no cookie -- set the user to be null in the res.locals
+    } else {
+        res.locals.user = null
+    }
+    // move on to the next route or middleware in the chain
+    next()
+})
 
 app.get("/",(req,res)=>{
     const url = "https://valorant-api.com/v1/weapons"
@@ -13,19 +38,15 @@ app.get("/",(req,res)=>{
     axios.get(url)
     .then(response=>{
         // console.log(response)
-        // res.render("home",{guns:response.data.data})
-        res.send(response.data.data)
+        res.render("home",{guns:response.data.data})
+        res.send()
     })
+    console.log('the currently logged in user is:', res.locals.user)
 })
-app.get("/1",(req,res)=>{
-    const url = "https://valorant-api.com/v1/weapons"
 
-    axios.get(url)
-    .then(response=>{
-        // console.log(response)
-        res.render("show",{guns:response.data.data})
-    })
-})
+// Controllers
+app.use("/guns",require("./controllers/guns"))
+app.use("/users",require("./controllers/users"))
 
 
 app.listen(PORT,()=>{
