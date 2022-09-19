9cb9f42befab73d0b1fd064a322f762855f7f184
JamesOnwordi <Jamesonwordi50@gmail.com>
2022-09-19 16:46:23 -0500

    deployment jam

--- controllers/video.js
+++ controllers/video.js
@@ -12,6 +12,7 @@ router.get("/",async (req,res)=>{
         const url = `https://valorant-api.com/v1/weapons`
 
         const response = await axios.get(url)
+        
         // console.log(response)
         const datas= response.data.data 
         // console.log("DATAS",datas)
@@ -19,7 +20,60 @@ router.get("/",async (req,res)=>{
             let store =[]
             datas.forEach(data =>{
                 data.skins.forEach(skin =>{
-                skin.chromas.forEach(chroma=>{  
+                skin.chromas.forEach( chroma=>{  
+                    // console.log("CHROMA",chroma)
+                    console.log("waiting")
+                    const reactions =  db.reaction.findAll({
+                        where:{
+                            uuid:chroma.uuid
+                        }
+                    })
+                    
+                    let liked =0
+                    let disliked =0
+                    if(reactions.data){
+                    reactions.data.forEach( reaction=>{
+
+                        let liked =liked ++
+                        let disliked =dislike ++
+                    })
+                    }
+                    
+                    console.log("HEREREFR")
+                         if(chroma.streamedVideo){  
+                            console.log("inside")
+                            store.push({ 
+                                name:chroma.displayName,
+                                video:chroma.streamedVideo,
+                                uuid:chroma.uuid,
+                                like:liked,
+                                dislike:disliked                 
+                            })   
+                        }
+                    })
+                }) 
+            })
+            console.log("store")
+            console.log(store)
+            res.render("armory/video",{skins:store,})
+        }catch(err){
+            console.log(err)
+        } 
+}) 
+
+router.get("/:id",async (req,res)=>{
+    try{
+        const url = `https://valorant-api.com/v1/weapons/${req.params.id}`
+
+        console.log("REQ.PARAM",url)
+
+        const response = await axios.get(url)
+        // console.log(response)
+        const datas= response.data.data.skins 
+        // console.log("DATAS",datas)
+            let store =[]
+            datas.forEach(data =>{
+                data.chromas.forEach(chroma=>{  
                     // console.log("CHROMA",chroma)
                          if(chroma.streamedVideo){  
                             store.push({
@@ -27,7 +81,6 @@ router.get("/",async (req,res)=>{
                                 video:chroma.streamedVideo
                             })   
                         }
-                    })
                 }) 
             })
             res.render("armory/video",{skins:store,})
@@ -59,6 +112,86 @@ router.get("/:id",async (req,res)=>{
                 }) 
             })
             res.render("armory/video",{skins:store,})
+        }catch(err){
+            console.log(err)
+        } 
+}) 
+
+router.post("/",async (req,res)=>{
+    console.log("REQ.params",req.params)
+    try{
+        const reaction = await db.reaction.findOne({
+            where:{
+                uuid:req.query.id,
+                userId:res.locals.user
+            }
+        })
+        if(reaction){
+            if(req.query.react == "like"){
+                if(reaction.like){
+                    if(reaction.like){
+                        await db.reaction.update({
+                            dislike: 0
+                            },{
+                            where:{
+                                uuid:req.query.id,
+                                userId:res.locals.user
+                            }
+                        })    
+                }else{
+                    await db.reaction.update({
+                        dislike: 0,
+                        like: 1
+                        },{
+                        where:{
+                            uuid:req.query.id,
+                            userId:res.locals.user
+                        }
+                    })
+                }
+            }
+            else{
+                if(reaction.dislike){
+                    await db.reaction.update({
+                        dislike: 0
+                        },{
+                        where:{
+                            uuid:req.query.id,
+                            userId:res.locals.user
+                        }
+                    })
+                }else{
+                    await db.reaction.update({
+                        dislike: 1,
+                        like: 0
+                        },{
+                        where:{
+                            uuid:req.query.id,
+                            userId:res.locals.user
+                        }
+                    })
+                }
+            }
+        }
+        }else{
+            // no previous reaction
+            if(req.query.react == "like"){
+            await db.reaction.create({
+                like:1,
+                uuid:req.query.id,
+                userId:res.locals.user
+            })}
+            else{
+                await db.reaction.create({
+                    dislike:1,
+                    uuid:req.query.id,
+                    userId:res.locals.user
+                })
+            }
+        }
+
+            res.redirect("/video")
+    
         }catch(err){
             console.log(err)
         } 
